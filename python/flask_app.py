7ebbf78f09640bee358d880e584fb0d9cce1d004
jmarusak <jmarusak@users.noreply.github.com>
2023-10-22 11:00:48 -0400

    flask caching for date persistence

--- python/flask_app.py
+++ python/flask_app.py
@@ -1,16 +1,23 @@
 import pickle
 from flask import Flask, request, render_template
+from flask_caching import Cache
 
 app = Flask(__name__)
+cache = Cache(config={'CACHE_TYPE': 'SimpleCache'})
+cache.init_app(app)
 
 @app.route('/' , methods=['GET', 'POST'])
 def clipboard():
-    form_data = pickle.load(open('form_data.p', 'rb'))
-
     if request.method == 'GET':
+        try:
+            form_data = cache.get('form_data')
+        except KeyError:
+            form_data=None
         return render_template('form.html', form_data=form_data)
 
     if request.method == 'POST':
         form_data = request.form
-        pickle.dump(form_data, open('form_data.p', 'wb'))
+        cache.set('form_data', form_data)
         return render_template('form.html', form_data=form_data)
+
+app.run(host='localhost', port=5000)
